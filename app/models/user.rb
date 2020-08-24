59af9d19f1bd2d44c55d70d148179aaf21d06051
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-08-24 09:03:05 +0900

    Revert "Revert "Create user.rb""
    This reverts commit 1b461bbb60e468e876f4b8182ddde466f258602b.

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+
+  validates_format_of :password ,with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze, message: 'Include both letters and numbers'
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+    validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' } 
+    validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' } 
+  end
+end
