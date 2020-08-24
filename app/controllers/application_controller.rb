ef0d20b84ff4e0d25e556e41007f969eb89d5356
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-08-24 09:03:02 +0900

    Revert "Revert "Update application_controller.rb""
    This reverts commit 85a1e49bdfad0b5b82e115e8e34b8a98ee586fae.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   
   private
 
@@ -7,6 +9,12 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials.basic[:user_name] && password == Rails.application.credentials.basic[:password]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
   end
 
 end
