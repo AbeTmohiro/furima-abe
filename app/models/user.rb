a8bab9364cec7c392481868559ca6335b265f3fc
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-08-25 16:09:29 +0900

    Create variables that REGEX

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_format_of :password ,with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze, message: 'Include both letters and numbers'
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  KANA_KANJI_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  KANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+
+  validates_format_of :password ,with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
   with_options presence: true do
     validates :nickname
     validates :birth_date
-    validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-    validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' } 
-    validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' } 
+    validates :first_name,format: { with: KANA_KANJI_REGEX, message: 'Full-width characters' }
+    validates :last_name,format: { with: KANA_KANJI_REGEX, message: 'Full-width characters' }
+    validates :first_name_kana, format: { with: KANA_REGEX, message: 'Full-width katakana characters' } 
+    validates :last_name_kana, format: { with: KANA_REGEX, message: 'Full-width katakana characters' } 
   end
 end
