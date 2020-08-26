e82fe862e243e4f1df9eccabe7b5f1fd7599a553
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-08-27 08:53:49 +0900

    Add validate and association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  with_options numericality: { other_than: 0, message: 'Select' } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_day
 end
