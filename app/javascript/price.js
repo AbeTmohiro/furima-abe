bd59a71be4de310d2a97809ad1b4e8b331ccfe61
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-08-27 09:40:40 +0900

    Add js files

---
+++ app/javascript/price.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const path = location.pathname
+  const pathRegex = /^(?=.*item)(?=.*edit)/
+  if (path === "/items/new" || path === "/items" || pathRegex.test(path)) {
+    const priceInput = document.getElementById('item-price');
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    priceInput.addEventListener('input', () => {
+      const inputRegex = /^([1-9]\d*|0)$/
+      const inputValue = document.getElementById("item-price").value;
+      if (inputRegex.test(inputValue)) {
+        addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+        profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+      } else {
+        addTaxDom.innerHTML = '半角数字のみ入力可能'
+        profitDom.innerHTML = '半角数字のみ入力可能'
+      }
+    })
+  }
+});
