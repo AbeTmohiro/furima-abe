26ed229e703ee8babe59c64076eacedadaa58014
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-08-27 09:32:19 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
     validates :delivery_day_id
   end
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee
