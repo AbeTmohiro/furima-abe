1f2109b5b61300048c3f0d57042d755096e06b7d
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-08-29 09:08:56 +0900

    Add association and validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,19 @@
 class Item < ApplicationRecord
-  with_options numericality: { other_than: 0, message: 'Select' } do
+  with_options numericality: { other_than: 0, message: 'Select please.' } do
     validates :category_id
     validates :condition_id
     validates :shipping_fee_id
     validates :prefecture_id
     validates :delivery_day_id
+  end
+
+  validates_inclusion_of :price, in: 300..9_999_999, message: 'Out of setting range'
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -13,4 +22,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_day
+
+  has_one_attached :image
+
+  belongs_to :user
 end
