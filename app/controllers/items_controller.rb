7c57c49f76e16a5f0eca8f81fbdb8b0f6dcc27fe
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-09-10 09:01:18 +0900

    add edit,update in select_item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :select_item, only: [:show]
+  before_action :select_item, only: [:show,:edit,:update]
   before_action :authenticate_user!, except: [:index,:show]
 
   def index
