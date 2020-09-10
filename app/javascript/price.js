ddf695b6adbdf5a66fcb94ad64e4799b785f6ca5
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-09-10 09:03:34 +0900

    Add edit_path for execute price.js

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,7 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
   const path = location.pathname
-  if (path === "/items/new" || path === "/items") {
-    //    出品ページの場合　||　出品ページの検証にかかった場合
+  const pathRegex = /^(?=.*item)(?=.*edit)/
+  if (path === "/items/new" || path === "/items" || pathRegex.test(path)) {
+    //    出品ページの場合　||　出品ページの検証にかかった場合 || 商品編集の場合
     const priceInput = document.getElementById('item-price');
     const addTaxDom = document.getElementById("add-tax-price");
     const profitDom = document.getElementById("profit");
