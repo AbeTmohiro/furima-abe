84668c80ffae8f0c79090155e92bb34f819e5eac
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-09-14 09:21:16 +0900

    Update transactions_controller.rb

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,4 +4,14 @@ class TransactionsController < ApplicationController
     @item = Item.find(params[:item_id])
     return redirect_to root_path if current_user.id == @item.user_id
   end
+
+  def create
+    @item_transaction = PayForm.new(item_transaction_params)
+    if @item_transaction.valid?
+      pay_item
+      @item_transaction.save
+      return redirect_to root_path
+    end
+    render 'index'
+  end
 end
